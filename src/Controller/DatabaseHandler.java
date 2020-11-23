8b490c617b0f41b207d5aa92c086358939cd4f73
sunnyz17 <zhang.sunny_1999@hotmail.com>
2020-11-22 17:45:49 -0800

    added selectCoordofAllPosts, selectUsername, insertPost functions

--- src/Controller/DatabaseHandler.java
+++ src/Controller/DatabaseHandler.java
@@ -1,5 +1,6 @@
 package src.Controller;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -8,21 +9,31 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-import src.Model.DatabaseModel;
+import src.Model.PostModel;
+import src.Model.AdministratorModel;
+import src.Model.CommentModel;
+import src.Model.LikedPostModel;
+import src.Model.ReplyToModel;
+import src.Model.TrendingTopicModel;
+import src.Model.UserModel;
 
 
 public class DatabaseHandler{
 
 	private static final String EXCEPTION_TAG = "[EXCEPTION]";
-	private static final String WARNING_TAG = "[WARNING]";
+	//private static final String WARNING_TAG = "[WARNING]";
  
     private Connection connection = null;
 
     // JDBC driver name and database URL
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://localhost/";
+
+    //this depends on ur database name so 
+    //URL   =====>   "jdbc:mysql://localhost:3306/" + [your database name]
+    static final String DB_URL = "jdbc:mysql://localhost:3306/new_schema";
 
     //  Database credentials
+    //this depends on your own set password
     static final String USER = "root";
     static final String PASS = "123123";
 
@@ -35,7 +46,7 @@ public class DatabaseHandler{
                     //STEP 2: Register JDBC driver
                     Class.forName("com.mysql.jdbc.Driver");
 
-                    //STEP 3: Open a connection
+                    // Open a connection
                     System.out.println("Connecting to database...");
                     conn = DriverManager.getConnection(DB_URL, USER, PASS);
 
@@ -60,7 +71,7 @@ public class DatabaseHandler{
                     }catch(SQLException se2){
                     }// nothing we can do
                     try{
-                        if(conn!=null)
+                        if(conn !=null)
                             conn.close();
                     }catch(SQLException se){
                         se.printStackTrace();
@@ -80,6 +91,15 @@ public class DatabaseHandler{
             }
         }
 
+        private void rollbackConnection() {
+            try  {
+                connection.rollback();	
+            } catch (SQLException e) {
+                System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+            }
+        }
+
+        //deletes all posts with ID >= 1000
         public void deletePost(int PostID){
             Connection conn = null;
             Statement stmt = null;
@@ -87,7 +107,7 @@ public class DatabaseHandler{
             try{
                 Class.forName("com.mysql.jdbc.Driver");
 
-                //STEP 3: Open a connection
+                // Open a connection
                 System.out.println("Connecting to database...");
                 conn = DriverManager.getConnection(DB_URL, USER, PASS);
                 stmt = conn.createStatement();
@@ -98,6 +118,7 @@ public class DatabaseHandler{
 
             }catch(SQLException ex){
                 ex.printStackTrace();
+                rollbackConnection();
             }catch(Exception e){
                 //Handle errors for Class.forName
                 e.printStackTrace();
@@ -107,18 +128,21 @@ public class DatabaseHandler{
             }catch(SQLException se){
                 se.printStackTrace();
             }//end try
-            System.out.println("FINISHED DELETING ALL POSTS");
+            System.out.println("FINISHED DELETING ALL POSTS WITH ID >= 1000");
         }
 
-        public void selectPost(){
+        //selects all the posts in the database
+        public PostModel[] selectPost(){
+            ArrayList<PostModel> result = new ArrayList<PostModel>();
             Connection conn = null;
             Statement stmt = null;
             try{
                     Class.forName("com.mysql.jdbc.Driver");
 
-                    //STEP 3: Open a connection
+                    //Open a connection
                     System.out.println("Connecting to database...");
                     conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                    connection = DriverManager.getConnection(DB_URL, USER, PASS);
                     stmt = conn.createStatement();
                 
                     //INSERT SQL SELECT STATEMENT HERE
@@ -129,24 +153,21 @@ public class DatabaseHandler{
                     int rowCount = 0;
                     
                     //here, you select each entitites from the Post table 
+                    //this loop gets all the rows in the Post table
                     while(rset.next()){
-                        Integer PostID = rset.getInt("PostID");
-                        String Content = rset.getString("Content");
-                        Integer UserID = rset.getInt("UserID");
-                        String time = rset.getString("timee");
-                        System.out.print("PostID is: " + PostID + ", " + "Post Content is: " + Content + ", " + "Posted by UserID: " + UserID + ", " + "Posted at the time: " + time);
-                        ++rowCount;
+                        PostModel model = new PostModel(
+                            rset.getInt("PostID"),
+                            rset.getString("Content"),
+                            rset.getInt("UserID"),
+                            rset.getString("timee"));
+
+                        System.out.println(model + "\n");
+                        result.add(model);
                     }
-                    //ie
-                    // while(rset.next()) {   // Move the cursor to the next row, return false if no more row
-                    //     String title = rset.getString("title");
-                    //     double price = rset.getDouble("price");
-                    //     int    qty   = rset.getInt("qty");
-                    //     System.out.println(title + ", " + price + ", " + qty);
-                    //     ++rowCount;
-                    //  }
+               
                 }catch(SQLException ex){
                     ex.printStackTrace();
+                    rollbackConnection();
                 }catch(Exception e){
                     //Handle errors for Class.forName
                     e.printStackTrace();
@@ -156,29 +177,35 @@ public class DatabaseHandler{
                 }catch(SQLException se){
                     se.printStackTrace();
                 }//end try
+
+                
                 System.out.println("finished select!");
+                return result.toArray(new PostModel[result.size()]);
         }
 
-
-        public void insertPost(){
+        //inserts a post specified by a user
+        public void insertPost(PostModel model){
             Connection conn = null;
-            Statement stmt = null;
             try{
                     Class.forName("com.mysql.jdbc.Driver");
 
-                    //STEP 3: Open a connection
+                    // Open a connection
                     System.out.println("Connecting to database...");
                     conn = DriverManager.getConnection(DB_URL, USER, PASS);
-                    stmt = conn.createStatement();
 
-                    //TODO: 
-                    //DO THE INSERTING CODE HERE
-                    //NEED TO CONNECT TO THE FRONT END OPTIONS SOMEHOW?
+                    PreparedStatement ps = conn.prepareStatement("INSERT INTO Post VALUES (?,?,?,?)");
 
+                    ps.setInt(1, model.getPostID());
+                    ps.setString(2, model.getContent());
+                    ps.setInt(3, model.getUserID());
+                    ps.setString(4, model.getTime());
 
+                    ps.executeUpdate();
 
+                    ps.close();
             }catch(SQLException ex){
                 ex.printStackTrace();
+                rollbackConnection();
             }catch(Exception e){
                 //Handle errors for Class.forName
                 e.printStackTrace();
@@ -191,6 +218,91 @@ public class DatabaseHandler{
         }
 
 
+
+        //gets the coordinates of all the posts
+        public void selectCoordofAllPosts(){
+            Connection conn = null;
+            Statement stmt = null;
+            try{
+                    Class.forName("com.mysql.jdbc.Driver");
+
+                    // Open a connection
+                    System.out.println("Connecting to database...");
+                    System.out.println("Selecting all posts coords");
+                    conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                    stmt = conn.createStatement();
+
+                    String strSelect = "SELECT Latitude, Longtitude, PostID FROM LocationOf";
+                    ResultSet rset = stmt.executeQuery(strSelect);
+                    int rowCount = 0;
+
+                    //TODO:
+                    while(rset.next()){
+                        BigDecimal Lat = rset.getBigDecimal("Latitude");
+                        BigDecimal Long = rset.getBigDecimal("Longtitude");
+                        Integer PostID = rset.getInt("PostID");
+                        System.out.println("row: " + rowCount + "PostID is :" + PostID + ", " + "With Latitude and Longtitude: " + Lat + ", " + Long + + "\n");
+                        ++rowCount;
+                    }
+
+
+            }catch(SQLException ex){
+                ex.printStackTrace();
+                rollbackConnection();
+            }catch(Exception e){
+                //Handle errors for Class.forName
+                e.printStackTrace();
+            }try{
+                if(conn!=null)
+                    conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }//end try
+        }
+
+
+
+        //gets all the usernames of users 
+        public String[] selectUsername(){
+            ArrayList<String> result = new ArrayList<String>();
+            Connection conn = null;
+            Statement stmt = null;
+            try{
+                    Class.forName("com.mysql.jdbc.Driver");
+
+                    //Open a connection
+                    System.out.println("Connecting to database...");
+                    System.out.println("Selecting all Usernames");
+                    conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                    stmt = conn.createStatement();
+
+
+                    String strSelect = "SELECT Username FROM User";
+                    ResultSet rset = stmt.executeQuery(strSelect);
+                    int rowCount = 0;
+
+                    while(rset.next()){
+                        String Username = rset.getString("Username");
+                        System.out.println("User: " + rowCount + " " + Username + "\n");
+                        result.add(Username);
+                        ++rowCount;
+                    }
+
+
+            }catch(SQLException ex){
+                ex.printStackTrace();
+                rollbackConnection();
+            }catch(Exception e){
+                //Handle errors for Class.forName
+                e.printStackTrace();
+            }try{
+                if(conn!=null)
+                    conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }//end try
+            return result.toArray(new String[result.size()]);
+        }
 
 
 
